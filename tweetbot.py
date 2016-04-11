f36cc84fbf747d4bf7dbc077d6a360772f72a9c0
rakhi09 <rakhiharkawat@gmail.com>
2016-04-10 18:56:09 -0700

    Fixed code

---
+++ tweetbot.py
@@ -0,0 +1,41 @@
+from twython import Twython, TwythonError
+import time
+
+#Api info goes here
+APP_KEY = ‘XXXXXXXXX’
+APP_SECRET = 'XXXXXXXXX'
+OAUTH_TOKEN = 'XXXXXXXXX-kIqyYFpDzRjPzAHHymG1mqWQzP8nzJn'
+OAUTH_TOKEN_SECRET = 'XXXXXXXXX'
+
+#Use Twython to set up api access to twitter
+api = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+try:
+	#Read the things we need to tweet
+	with open('tweets.txt', 'r+') as tweetsfile:
+		tweets = tweetsfile.readlines()
+
+	#Dictionary to maintain all tweets done
+	dict = {}
+
+	#Loop through all the tweets
+	for line in tweets[:]:
+		#Api does not support empty line, duplicate status and tweets > 140, thus ignoring those lines
+		if (line not in ['\n','\r\n']) and (line not in dict) and (len(line) <= 140):
+			#What are we tweeting
+			print ("Trying to tweet " + line)
+			#Attempt to send the tweet
+			api.update_status(status=str(line))
+			#Add it to dictionary
+			dict[line]=1
+			#Rate limit 
+			time.sleep(1)
+	#Yey it worked
+	print ("I have tweeted AllTheThings!") 
+
+except TwythonError, e:
+	#in case it didn't work
+	print("Something Broke :(")  
+	print('Failed '+ str(e))
+
+	
